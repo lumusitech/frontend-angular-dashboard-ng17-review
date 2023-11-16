f91cea3a1da2fd50c16c415febecf156ed3d0706
lumusitech <lumusitech@gmail.com>
2023-11-16 16:16:31 -0300

    feat: add menuItems from routes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+import { routes } from '../app.routes';
 
 @Component({
   standalone: true,
@@ -9,4 +10,14 @@ import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
-export default class DashboardComponent {}
+export default class DashboardComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route.path !== '**')
+    .filter((route) => !route.path?.includes('user/:id'));
+
+  constructor() {
+    console.log(this.menuItems);
+  }
+}
