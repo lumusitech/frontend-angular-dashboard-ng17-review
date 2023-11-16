e7c728bfbe1a59a11cf96927c6450bd7d222c87e
lumusitech <lumusitech@gmail.com>
2023-11-16 17:46:13 -0300

    feat: add functional sidemenu

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
-import { routes } from '../app.routes';
 
 @Component({
   standalone: true,
@@ -10,14 +9,4 @@ import { routes } from '../app.routes';
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
-export default class DashboardComponent {
-  public menuItems = routes
-    .map((route) => route.children ?? [])
-    .flat()
-    .filter((route) => route.path !== '**')
-    .filter((route) => !route.path?.includes('user/:id'));
-
-  constructor() {
-    console.log(this.menuItems);
-  }
-}
+export default class DashboardComponent {}
--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { routes } from '../../app.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'shared-sidemenu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidemenu.component.html',
   styles: ``,
 })
-export class SidemenuComponent {}
+export class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route.path !== '**')
+    .filter((route) => !route.path?.includes('user/:id'));
+}
