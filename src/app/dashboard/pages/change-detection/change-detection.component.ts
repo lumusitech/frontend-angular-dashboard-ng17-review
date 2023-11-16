59c753b147e201e72c9c4a4e3c1f1eccfb741ebd
lumusitech <lumusitech@gmail.com>
2023-11-16 20:54:16 -0300

    feat: add change detection with signal and unPush strategy

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   selector: 'dashboard-change-detection',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <div class="animate__animated animate__fadeIn">
+      <app-title [title]="currentFramework()" />
+
+      <pre>{{ frameworkAsSignal() | json }}</pre>
+      <pre>{{ frameworkAsProperty | json }}</pre>
+    </div>
+  `,
   styles: ``,
 })
-export default class ChangeDetectionComponent {}
+export default class ChangeDetectionComponent {
+  public currentFramework = computed(
+    () => `Change detenction: ${this.frameworkAsSignal().name}`
+  );
+
+  // Signal way of declaring a property
+  public frameworkAsSignal = signal({
+    type: 'signal with onPush strategy - zoneless',
+    name: 'Angular',
+    releaseDate: '2016',
+  });
+
+  // Traditional way of declaring a property
+  public frameworkAsProperty = {
+    type: "traditional way with zone.js can't be changed with onPush strategy",
+    name: 'Angular',
+    releaseDate: '2016',
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // change detection with zone.js
+      // using onPush this property can't be changed
+      // this.frameworkAsProperty.name = 'React';
+
+      // But with signal we can change it and is all so rapid
+      this.frameworkAsSignal.update((v) => ({
+        ...v,
+        name: 'React',
+      }));
+
+      console.log('ready!');
+    }, 3000);
+  }
+}
