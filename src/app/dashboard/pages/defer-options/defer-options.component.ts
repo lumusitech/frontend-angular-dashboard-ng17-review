84f9131d48a737e42537fdbda57e96d043e99bc6
lumusitech <lumusitech@gmail.com>
2023-11-17 01:38:13 -0300

    feat: add defer options

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { HeavyLoadersFastComponent } from '../../../shared/heavy-loaders/heavy-loaders-fast.component';
 import { TitleComponent } from '../../../shared/title/title.component';
 
 @Component({
   selector: 'dashboard-defer-options',
   standalone: true,
-  imports: [CommonModule, TitleComponent],
+  imports: [CommonModule, TitleComponent, HeavyLoadersFastComponent],
   templateUrl: './defer-options.component.html',
   styles: ``,
 })
--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-heavy-loaders-fast',
+  selector: 'shared-heavy-loaders-fast',
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>heavy-loaders-fast works!</p> `,
+  template: `
+    <section [ngClass]="['w-100', cssClass]">
+      <!-- ng-content allows receiving child components - High Order Component - HOC -->
+      <ng-content />
+    </section>
+  `,
 })
-export class HeavyLoadersFastComponent {}
+export class HeavyLoadersFastComponent {
+  @Input({ required: true }) cssClass!: string;
+
+  constructor() {
+    console.log('Heavy loader fast builded');
+  }
+}
